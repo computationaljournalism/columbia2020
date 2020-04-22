4572f719df48d38519c0ae340fd395ac08b0508a
Michael Young <michael.young@nytimes.com>
2020-04-22 13:36:53 -0400

    Some moving around to match Mark's prod setup. Adding sheet name (to sheets.py). Quick cleanup to Oakland County

--- michigan/oakland_county/process.py
+++ michigan/oakland_county/process.py
@@ -13,74 +13,79 @@ import sheets
 from requests import get
 from bs4 import BeautifulSoup
 from datetime import datetime
-
-now = datetime.now()
-
-log = open("log.txt","a")
-log.write("=================================\n")
-log.write("0. "+now.strftime("%Y-%m-%d-%H-%M")+"\n")
-
-#Finding the data from a map in this page https://www.oakgov.com/covid/casesByZip.html. 
-#Url3 is the address where the map pulls its data from
-
-url3='https://services1.arcgis.com/GE4Idg9FL97XBa3P/arcgis/rest/services/COVID19_Cases_by_Zip_Code_Total_Population/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Join_Zip_Code%20asc&resultOffset=0&resultRecordCount=200&cacheHint=true'
-
-response = get(url3)
-data=response.json()
-
-log.write("1. Pulled JSON\n")
-
-#The data is in the list of dictionaries called 'features'
-features=data['features']
-
-#We want to pull all the variables inside the dict called attributes, so we create a new list of dictionaries that only includes the attributes
-new_data=[]
-
-for feature in features:
-    new_data.append(feature['attributes'])
-
 from pandas import DataFrame
 
-Oakland = DataFrame(new_data)
-log.write("2. Created data frame\n")
+def main():
+    ''' run it
+    '''
+    now = datetime.now()
 
+    log = open("log.txt","a")
+    log.write("=================================\n")
+    log.write("0. "+now.strftime("%Y-%m-%d-%H-%M")+"\n")
 
-#### Output ##################
-utils.save_source_file("Oakland.json", response.text)
-log.write("3. Saved Source file\n")
+    #Finding the data from a map in this page https://www.oakgov.com/covid/casesByZip.html. 
+    #Url3 is the address where the map pulls its data from
 
-csv = Oakland.to_csv(index=False)
-utils.save_data_file("Oakland.csv", csv)
-log.write("4. Saved Data file\n")
+    url3 = 'https://services1.arcgis.com/GE4Idg9FL97XBa3P/arcgis/rest/services/COVID19_Cases_by_Zip_Code_Total_Population/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Join_Zip_Code%20asc&resultOffset=0&resultRecordCount=200&cacheHint=true'
 
-fp = Oakland[["Join_Zip_Code","Total__COVID19_Cases","Death_Count","Total_Population_ACS_17_5YR"]].copy(deep=True)
+    response = get(url3)
+    data = response.json()
 
-fp.columns=["ZIP code","Cases","Deaths","Population"]
-fp.loc[:,"Cases"] = fp["Cases"].astype(float)
-fp.loc[:,"Deaths"] = fp["Deaths"].astype(float)
-fp.loc[:,"Population"] = fp["Population"].astype(float)
-fp.loc[fp["Cases"].isnull(),"Cases"] = 0.0
-fp.loc[fp["Deaths"].isnull(),"Deaths"] = 0.0
-fp.loc[:,"Cases per 100,000"] = 100000.0*fp["Cases"]/fp["Population"]
-log.write("5. Processed Freepress\n")
+    log.write("1. Pulled JSON\n")
 
-fpcsv = fp.to_csv(index=False)
-utils.save_freepress_file("Oakland.csv", fpcsv)
-log.write("6. Saved Freepress\n")
-##############################
+    #The data is in the list of dictionaries called 'features'
+    features = data['features']
 
-#### save csv to sheets ######
+    #We want to pull all the variables inside the dict called attributes, so we create a new list of dictionaries that only includes the attributes
+    new_data = []
 
-GOOGLE_SHEET_ID = '1wtkFL6BIZQoLnUVfcr8y_2XyjSzlrPimH6A0lMjtlys'
-try:
-    # we need to convert the dataframe to a list of lists
-    # and then update via google sheets api
-    data_to_save = [fp.columns.values.tolist()] + fp.values.tolist() 
-    sheets.update_sheet(GOOGLE_SHEET_ID, data_to_save)
-    log.write("7. Updated google sheet\n")
+    for feature in features:
+        new_data.append(feature['attributes'])
 
-except Exception as exc:
-    print(exc)
-    log.write(f'exception writing to sheet: {exc}')
+    Oakland = DataFrame(new_data)
+    log.write("2. Created data frame\n")
 
-##############################
+    #### Output ##################
+    utils.save_source_file("Oakland.json", response.text)
+    log.write("3. Saved Source file\n")
+
+    csv = Oakland.to_csv(index=False)
+    utils.save_data_file("Oakland.csv", csv)
+    log.write("4. Saved Data file\n")
+
+    fp = Oakland[["Join_Zip_Code","Total__COVID19_Cases","Death_Count","Total_Population_ACS_17_5YR"]].copy(deep=True)
+
+    fp.columns=["ZIP code","Cases","Deaths","Population"]
+    fp.loc[:,"Cases"] = fp["Cases"].astype(float)
+    fp.loc[:,"Deaths"] = fp["Deaths"].astype(float)
+    fp.loc[:,"Population"] = fp["Population"].astype(float)
+    fp.loc[fp["Cases"].isnull(),"Cases"] = 0.0
+    fp.loc[fp["Deaths"].isnull(),"Deaths"] = 0.0
+    fp.loc[:,"Cases per 100,000"] = 100000.0*fp["Cases"]/fp["Population"]
+    log.write("5. Processed Freepress\n")
+
+    fpcsv = fp.to_csv(index=False)
+    utils.save_freepress_file("Oakland.csv", fpcsv)
+    log.write("6. Saved Freepress\n")
+    ##############################
+
+    #### save csv to sheets ######
+
+    GOOGLE_SHEET_ID = '1wtkFL6BIZQoLnUVfcr8y_2XyjSzlrPimH6A0lMjtlys'
+    GOOGLE_SHEET_NAME = 'Oakland County'
+    try:
+        # we need to convert the dataframe to a list of lists
+        # and then update via google sheets api
+        data_to_save = [fp.columns.values.tolist()] + fp.values.tolist() 
+        sheets.update_sheet(GOOGLE_SHEET_ID, GOOGLE_SHEET_NAME, data_to_save)
+        log.write("7. Updated google sheet\n")
+
+    except Exception as exc:
+        print(exc)
+        log.write(f'exception writing to sheet: {exc}')
+
+    ##############################
+
+if __name__ == '__main__':
+    main()
